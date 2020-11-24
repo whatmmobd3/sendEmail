111d44df4a6f5519dc25e4dc8949f0d915defcf3
Your Name <you@example.com>
2020-11-24 13:25:22 +0700

    update

--- app.js
+++ app.js
@@ -16,41 +16,64 @@ const fs = require("fs");
 
 const directoryPath = "./file";
 
-app.get("/", (req, res) => { });
+app.get("/", (req, res) => {});
 
 app.post("/", async (req, res) => {
   let transporter = nodemailer.createTransport(config);
-  let countEmail = 0;
-  let countFile = fs.readdirSync(directoryPath)
+  var countEmail = 0;
+  // let countFile = fs.readdirSync(directoryPath);
 
-  fs.readdirSync(directoryPath).forEach((file) => {
-    let pathAttachment = `${directoryPath}/${file}`;
+  let aa = await fs.readdirSync(directoryPath);
+
+
+  aa.map((e) => {
     let mailOptions = {
       to: req.body.to,
       subject: req.body.title,
       text: req.body.body,
       attachments: [
         {
-          // path: "./file/a.pdf",
-          path: pathAttachment
+          path: `${directoryPath}/${e}`
         },
       ],
     };
+
     transporter.sendMail(mailOptions, (err, info) => {
       if (err) {
-        console.log('-----', err);
+        console.log("-----", err);
       } else {
-        countEmail++
-        console.log('+++++', info);
+        countEmail++;
+        console.log("+++++", countEmail);
       }
-    }
-    );
+    });
   });
 
+  console.log(countEmail);
+  // fs.readdirSync(directoryPath).forEach((file) => {
+  //   let mailOptions = {
+  //     to: req.body.to,
+  //     subject: req.body.title,
+  //     text: req.body.body,
+  //     attachments: [
+  //       {
+  //         // path: "./file/a.pdf",
+  //         path: pathAttachment
+  //       },
+  //     ],
+  //   };
+  //   transporter.sendMail(mailOptions, (err, info) => {
+  //     if (err) {
+  //       console.log('-----', err);
+  //     } else {
+  //       countEmail++
+  //       console.log('+++++', countEmail);
+  //     }
+  //   }
+  //   );
+  // });
+
   await res.send(rs.SUCCESS(countEmail));
-
 });
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
